7cf794bf1b7ea261b83afa6a1f648ffa8ecf37a6
193s <193sim@gmail.com>
2014-11-15 18:51:35 +0900

    fix usage

--- src/HashCal.scala
+++ src/HashCal.scala
@@ -1,32 +1,39 @@
 
 object HashCal {
-    def main(args: Array[String]) {
-        args.length match {
-            case 1 => {
-                if (args(0) == "-md5") showUsage
-                else println(args(0).hashCode)
-            }
-            case 2 => {
-                args(0) match {
-                    case "-md5" => println(md5Hash(args(1)))
-                    case _ => showUsage
-                }
-            }
-            case _ => showUsage
-        }
-    }
+  def main(args: Array[String]) {
+    args.length match {
+      case 1 => 
+        if (args(0) == "-md5") showUsage
+        else println(args(0).hashCode)
 
-    def showUsage(): Unit = {
-        println("Usage: scala HashCalc <string>\n"
-              + "     | scala HashCalc -md5 <string>")
+      case 2 => 
+        args(0) match {
+          case "-md5" => println(md5hash(args(1)))
+          case "-a" => interactive()
+          case _ => showUsage
+        }
+      case _ => showUsage
     }
+  }
+
+  def showUsage(): Unit = {
+    println("Usage: hashcal <string>\n"
+          + "     | scala HashCalc -md5 <string>")
+  }
  
 
-    def md5Hash(text: String): String = {
-        java.security.MessageDigest.getInstance("MD5")
-        .digest(text.getBytes)
-        .map(0xFF & _)
-        .map { "%02x".format(_) }
-        .foldLeft(""){_ + _}
-    }
+  def md5hash(text: String): String = {
+    java.security.MessageDigest.getInstance("MD5")
+    .digest(text.getBytes)
+    .map(0xFF & _)
+    .map { "%02x".format(_) }
+    .foldLeft(""){_ + _}
+  }
+
+  def interactive(): Unit = {
+    print("hashcal >> ")
+    Console.readLine()
+    
+
+  }
 }
